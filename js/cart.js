7e5ab4b62555e71a9b603aaa74b51819512ca281
Perrywzp <wangzhipei1121@163.com>
2017-04-14 16:57:12 +0800

    【add】完善购物车功能

--- js/cart.js
+++ js/cart.js
@@ -8,7 +8,9 @@ var vm = new Vue({
         productList:[]
     },
     filters: {
-
+        formatRmb: function(value, type){
+            return "￥"+value.toFixed(2)+type;
+        }
     },
     mounted: function(){
         this.cartView();
@@ -16,9 +18,21 @@ var vm = new Vue({
     methods: {
         cartView: function(){
             var _this = this;
-            this.$http.get("data/account.json", {"id": 22}).then(function(res){
-                _this.productList = res.result.list;
+            this.$http.get("data/cart.json", {"id": 22}).then(function(res){
+                _this.productList = res.body.result.productList;
+                _this.totalMoney = res.body.result.totalMoney;
             })
+        },
+        addCount: function(item, type){
+            if(type>0){
+                item.productQuentity++;
+            }else{
+                (item.productQuentity > 0) && item.productQuentity--;
+            }
+
+            this.totalMoney = this.productList.map((x)=>{
+                return x.productQuentity * x.productPrice;
+            }).reduce((x,y)=>{return x+y;})
         }
     }
 });
